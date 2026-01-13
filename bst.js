271b1f40ebe188700c48cd23b30dcd16d7f4eabe
= <lukejmullins@gmail.com>
2026-01-13 09:54:16 -0500

    Add build Tree

---
+++ bst.js
@@ -0,0 +1,50 @@
+class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Tree {
+  constructor(arr) {
+    this.arr = [...new Set(arr)].sort((a, b) => a - b);
+    this.root = this.buildTree(this.arr);
+  }
+
+  buildTree(array) {
+    let start = 0;
+    let end = array.length - 1;
+
+    if (start > end) return null;
+
+    let mid = Math.floor((start + end) / 2);
+    let root = new Node(array[mid]);
+
+    root.left = this.buildTree(array.slice(start, mid));
+
+    root.right = this.buildTree(array.slice(mid + 1, end + 1));
+
+    return root;
+  }
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const tree = new Tree(array);
+
+prettyPrint(tree.root);
